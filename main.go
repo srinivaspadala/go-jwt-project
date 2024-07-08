5844da257db16d0c3289d875094d56a0c82b089e
srinivaspadala <srinivaspadala.cse@gmail.com>
2024-07-08 18:24:01 +0530

    first commit

---
+++ main.go
@@ -0,0 +1,23 @@
+package main
+
+import (
+	routes "go-jwt-project/routes"
+	"os"
+
+	"github.com/gin-gonic/gin"
+)
+
+func main() {
+	port := os.Getenv("PORT")
+
+	if port == "" {
+		port = "8080"
+	}
+
+	router := gin.New()
+	router.Use(gin.Logger())
+
+	routes.AuthRoutes(router)
+	routes.UserRoutes(router)
+
+}
